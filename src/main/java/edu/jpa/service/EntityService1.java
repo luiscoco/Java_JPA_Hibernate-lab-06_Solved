2e8c664780b4c9787c4847bef4a9136abd9666c7
Luis Coco Enríquez <LEnriquez@luxoft.com>
2025-11-10 13:01:58 +0100

    first commit

---
+++ src/main/java/edu/jpa/service/EntityService1.java
@@ -0,0 +1,41 @@
+package edu.jpa.service;
+
+import edu.jpa.entity.Employee;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+public class EntityService1 extends EntityService {
+
+    @Override
+    public List<Employee> getEmployeesByDepartmentName(String name) {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        try {
+            em.getTransaction().begin();
+            String queryText = "select e from Employee e where e.department.name = :name";
+            TypedQuery<Employee> query = em.createQuery(queryText, Employee.class);
+            query.setParameter("name", name);
+            List<Employee> result = query.getResultList();
+            em.getTransaction().rollback();
+            return result;
+        } finally {
+            if (em.isOpen()) em.close();
+        }
+    }
+
+    @Override
+    public List<DepartmentInfo> getDepartmentsInfo() {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        try {
+            em.getTransaction().begin();
+            String queryText = "select new edu.jpa.service.DepartmentInfo(e.department.name,count(e.department)) from Employee e group by e.department.name";
+            TypedQuery<DepartmentInfo> query = em.createQuery(queryText, DepartmentInfo.class);
+            List<DepartmentInfo> result = query.getResultList();
+            em.getTransaction().rollback();
+            return result;
+        } finally {
+            if (em.isOpen()) em.close();
+        }
+    }
+}
---
+++ src/main/java/edu/jpa/service/EntityService2.java
@@ -0,0 +1,60 @@
+package edu.jpa.service;
+
+import edu.jpa.entity.Employee;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+
+public class EntityService2 extends EntityService {
+
+    @Override
+    public List<Employee> getEmployeesByDepartmentName(String name) {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+            Root<Employee> e = cq.from(Employee.class);
+            ParameterExpression<String> nameParam = cb.parameter(String.class, "name");
+            cq.where(cb.equal(e.get("department").get("name"), nameParam));
+
+            em.getTransaction().begin();
+            TypedQuery<Employee> query = em.createQuery(cq);
+            query.setParameter("name", name);
+            List<Employee> result = query.getResultList();
+            em.getTransaction().rollback();
+            return result;
+        } finally {
+            if (em.isOpen()) em.close();
+        }
+    }
+
+    @Override
+    public List<DepartmentInfo> getDepartmentsInfo() {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+
+            CriteriaQuery<DepartmentInfo> cq = cb.createQuery(DepartmentInfo.class);
+            Root<Employee> e = cq.from(Employee.class);
+            cq.select(cb.construct(
+                    DepartmentInfo.class,
+                    e.get("department").get("name"),
+                    cb.count(e.get("department"))
+            ));
+            cq.groupBy(e.get("department"));
+
+            em.getTransaction().begin();
+            TypedQuery<DepartmentInfo> query = em.createQuery(cq);
+            List<DepartmentInfo> result = query.getResultList();
+            em.getTransaction().rollback();
+            return result;
+        } finally {
+            if (em.isOpen()) em.close();
+        }
+    }
+}
